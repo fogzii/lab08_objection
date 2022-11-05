2d91e4268d1aaa54c66b4d60957eb34daf336c04
Bowen Xiao <z5416195@ad.unsw.edu.au>
2022-11-05 17:21:45 +1100

    basic testing

--- src/objection.test.ts
+++ src/objection.test.ts
@@ -1,11 +1,5 @@
 import { getObjections, Objection, ExaminationType } from './objection';
 
-test('Remove this test and uncomment the tests below', () => {
-  expect(1 + 1).toBe(2);
-});
-
-/*
-
 describe('argumentative cases', () => {
   test.each([
     {
@@ -25,4 +19,128 @@ describe('argumentative cases', () => {
   });
 });
 
-*/
+describe('compound cases', () => {
+  test.each([
+    {
+      question: 'compound? yes?',
+      testimony: 'yes you!',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.COMPOUND]),
+    },
+    {
+      question: 'compound? yes question is long apple pear banana?',
+      testimony: 'Yes, so not argumentative!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.COMPOUND]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('hearsay cases', () => {
+  test.each([
+    {
+      question: 'question me question is long apple pear banana?',
+      testimony: 'Your mum heard from me.',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.HEARSAY]),
+    },
+    {
+      question: 'question me question is long apple pear banana?',
+      testimony: 'Your mum told me.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.HEARSAY]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('leading cases', () => {
+  test.each([
+    {
+      question: 'why did you eat that lemon?',
+      testimony: 'lemon yum.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'do you agree?',
+      testimony: 'agree no.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'you like lemons, right?',
+      testimony: 'lemons yes.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'you like lemons, correct?',
+      testimony: 'lemons no.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('non responsive cases', () => {
+  test.each([
+    {
+      question: 'question is long apple pear banana?',
+      testimony: 'lemon.',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.NON_RESPONSIVE]),
+    },
+    {
+      question: 'question is long apple pear banana?',
+      testimony: 'lemon quest.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.NON_RESPONSIVE]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('relevance cases', () => {
+  test.each([
+    {
+      question: 'question?',
+      testimony: 'lemon. This is longer than the question.',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.RELEVANCE]),
+    },
+    {
+      question: 'question?',
+      testimony: 'lemon quest. This is longer than the question.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.RELEVANCE]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('speculation cases', () => {
+  test.each([
+    {
+      question: 'question with speculation?',
+      testimony: 'think yes.',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.SPECULATION]),
+    },
+    {
+      question: 'question with speculation?',
+      testimony: 'yes think.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.SPECULATION]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
